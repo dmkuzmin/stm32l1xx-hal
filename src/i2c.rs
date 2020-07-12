7caf5c37060d8d431ed593e766be30a97faabad1
dmkuzmin <kuzmin.d@redbees.ru>
2020-07-12 14:55:05 +0300

    1) I2C1 pins to PB8 and PB9

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,7 +1,7 @@
 //! I2C
 use hal::blocking::i2c::{Read, Write, WriteRead};
 
-use crate::gpio::gpiob::{PB10, PB11, PB6, PB7};
+use crate::gpio::gpiob::{PB10, PB11, PB8, PB9};
 use crate::gpio::{AltMode, OpenDrain, Output};
 use crate::prelude::*;
 use crate::rcc::Rcc;
@@ -18,7 +18,7 @@ pub trait Pins<I2c> {
     fn setup(&self);
 }
 
-impl Pins<I2C1> for (PB6<Output<OpenDrain>>, PB7<Output<OpenDrain>>) {
+impl Pins<I2C1> for (PB8<Output<OpenDrain>>, PB9<Output<OpenDrain>>) {
     fn setup(&self) {
         self.0.set_alt_mode(AltMode::I2C);
         self.1.set_alt_mode(AltMode::I2C);
