6ed557edb32bb440ccff7a97bc3f17457478e9b7
dmkuzmin <kuzmin.d@redbees.ru>
2020-07-17 19:42:54 +0300

    no message

--- src/lib.rs
+++ src/lib.rs
@@ -34,19 +34,11 @@ pub use crate::stm32::interrupt;
 
 mod bb;
 
-pub mod adc;
-pub mod dac;
 pub mod delay;
-pub mod dma;
 pub mod exti;
 pub mod gpio;
 pub mod i2c;
 pub mod prelude;
-pub mod pwm;
-pub mod qei;
 pub mod rcc;
-pub mod serial;
-pub mod spi;
 pub mod time;
 pub mod timer;
-pub mod watchdog;
